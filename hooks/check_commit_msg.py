9ddec793171f51a25b4162f074f19196c437fd97
p0lygun <solankivibhakar82@gmail.com>
2022-07-21 00:17:40 +0530

    feat: complete commit-msg-check pre-commit hook

---
+++ hooks/check_commit_msg.py
@@ -0,0 +1,33 @@
+"""Hook to Check if a commit message is valid."""
+import sys
+
+from loguru import logger
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    colorize=True,
+    level="INFO",
+    format="<d>{message}</>"
+)
+
+keywords = [
+    'feat', 'fix', 'chore', 'refactor', 'docs',
+    'style', 'test', 'perf', 'ci', 'build', 'revert'
+]
+
+
+def main(*args):
+    """Check if a commit message is valid."""
+    with open(sys.argv[1]) as FILE:
+        data = FILE.read()
+        msg = data.split('\n')[0]
+        keyword = msg.split(':', maxsplit=1)[0]
+        if keyword not in keywords:
+            logger.info(
+                "\tCommit message must start with a keyword" +
+                f"\n\tvalid keywords {*keywords,}\n" +
+                "see \n" +
+                f"\n\tCurrent keyword is {keyword} \n"
+            )
+            sys.exit()
