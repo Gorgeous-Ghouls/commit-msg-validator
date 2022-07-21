e612026fae311a8489d3dfff383b894cc67bbe9a
p0lygun <solankivibhakar82@gmail.com>
2022-07-21 19:40:17 +0530

    feat: add tests

---
+++ tests/test_check_commit_msg.py
@@ -0,0 +1,24 @@
+"""Test file for commit-msg tests."""
+from unittest.mock import patch
+
+import pytest
+
+from hooks.check_commit_msg import main
+
+
+def test_check_commit_msg(tmp_path):
+    """Test for commit-msg pre-commit hook."""
+    commit_msg_file = tmp_path / 'msg'
+    patched_argv = ["anything", f"{commit_msg_file}"]
+    with patch('sys.argv', patched_argv):
+        with pytest.raises(SystemExit):
+            commit_msg = "wrong: keyword \n exit with \n 1"
+            commit_msg_file.write_text(commit_msg)
+            main()
+        try:
+            commit_msg = "feat: correct keyword \n exit with \n 0"
+            commit_msg_file.write_text(commit_msg)
+            main()
+        except SystemExit:
+            assert False, \
+                f"pre-hook raised SystemExit for commit-msg {commit_msg}"
